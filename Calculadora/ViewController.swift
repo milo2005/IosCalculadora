e59dea09ec2f12a88b22044637ec7261a6ef7868
Dario Chamorro <dario.chamorro.v@gmail.com>
2017-10-12 12:12:37 -0500

    Proyect Finished

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -10,16 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var num1:UITextField!
+    @IBOutlet var num2:UITextField!
+    @IBOutlet var result:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func sum(_ sender:UIButton){
+        let (n1,n2) = getOperators()
+        result.text = "\(n1 + n2)"
+    }
+    
+    @IBAction func sub(_ sender:UIButton){
+        let (n1,n2) = getOperators()
+        result.text = "\(n1 - n2)"
+    }
 
+    @IBAction func mul(_ sender:UIButton){
+        let (n1,n2) = getOperators()
+        result.text = "\(n1 * n2)"
+    }
+    
+    @IBAction func div(_ sender:UIButton){
+        let (n1,n2) = getOperators()
+        result.text = "\(n1 / n2)"
+    }
+    
+    func getOperators()->(n1:Int, n2:Int){
+        let nString1 = num1.text!
+        let nString2 = num2.text!
+        return (n1: Int(nString1)!, n2: Int(nString2)!)
+    }
+    
+    
+    
 
 }
 
